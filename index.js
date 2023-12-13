3e1ac92472052d190fdb57ddabf0ad522364bac4
Lee Bowyer <vanillacx@outlook.com>
2023-12-14 00:01:07 +0100

    Exposed KEK

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {Authenticator} = require("./modules/Authenticator");
 const {User} = require("./modules/User");
+const KEK = require("./modules/KEK");
 
-module.exports = {Authenticator, User}
+module.exports = {Authenticator, User, KEK}
